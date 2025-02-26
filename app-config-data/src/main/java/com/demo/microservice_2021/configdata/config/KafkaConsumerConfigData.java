42fe8065ee6dfb0a6d7e1d595a2cd57628fb624d
mohsen.mahmoudi <mohsen.mahmoudi.91@gmail.com>
2025-02-26 12:01:22 +0330

    add kafka consumer config data

---
+++ app-config-data/src/main/java/com/demo/microservice_2021/configdata/config/KafkaConsumerConfigData.java
@@ -0,0 +1,29 @@
+package com.demo.microservice_2021.configdata.config;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@ToString
+@Configuration
+@ConfigurationProperties(prefix = "kafka-consumer-config")
+public class KafkaConsumerConfigData {
+    private String keyDeserializer;
+    private String valueDeserializer;
+    private String consumerGroupId;
+    private String autoOffsetReset;
+    private String specificAvroReaderKey;
+    private String specificAvroReader;
+    private Boolean batchListener;
+    private Boolean autoStartup;
+    private Integer concurrencyLevel;
+    private Integer sessionTimeoutMs;
+    private Integer heartbeatIntervalMs;
+    private Integer maxPollIntervalMs;
+    private Integer maxPollRecords;
+    private Integer maxPartitionFetchBytesDefault;
+    private Integer maxPartitionFetchBytesBoostFactor;
+    private Long pollTimeoutMs;
+}
