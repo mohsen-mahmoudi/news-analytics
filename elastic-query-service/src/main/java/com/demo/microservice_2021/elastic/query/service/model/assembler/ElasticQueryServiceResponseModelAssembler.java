8fd4af5708cd62ffda074eab375c76add343edeb
mohsen.mahmoudi <mohsen.mahmoudi.91@gmail.com>
2025-03-12 12:31:42 +0330

    add hateoas to response model

---
+++ elastic-query-service/src/main/java/com/demo/microservice_2021/elastic/query/service/model/assembler/ElasticQueryServiceResponseModelAssembler.java
@@ -0,0 +1,41 @@
+package com.demo.microservice_2021.elastic.query.service.model.assembler;
+
+import com.demo.microservice_2021.elastic.model.index.impl.NewsIndexModel;
+import com.demo.microservice_2021.elastic.query.service.api.ElasticDocumentApi;
+import com.demo.microservice_2021.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.demo.microservice_2021.elastic.query.service.transformer.ElasticToResponseModelTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class ElasticQueryServiceResponseModelAssembler extends
+        RepresentationModelAssemblerSupport<NewsIndexModel, ElasticQueryServiceResponseModel> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ElasticDocumentApi.class);
+
+    private final ElasticToResponseModelTransformer transformer;
+
+    public ElasticQueryServiceResponseModelAssembler(ElasticToResponseModelTransformer transformer) {
+        super(ElasticDocumentApi.class, ElasticQueryServiceResponseModel.class);
+        this.transformer = transformer;
+    }
+
+    @Override
+    public ElasticQueryServiceResponseModel toModel(NewsIndexModel entity) {
+        ElasticQueryServiceResponseModel responseModel = transformer.getResponseModel(entity);
+        responseModel.add(linkTo(methodOn(ElasticDocumentApi.class).getDocumentById(entity.getId())).withSelfRel());
+        responseModel.add(linkTo(ElasticDocumentApi.class).withRel("documents"));
+        return responseModel;
+    }
+
+    public List<ElasticQueryServiceResponseModel> toModels(List<NewsIndexModel> entity) {
+        return entity.stream().map(this::toModel).toList();
+    }
+}
--- elastic-query-service/src/main/java/com/demo/microservice_2021/elastic/query/service/service/NewsElasticQueryService.java
+++ elastic-query-service/src/main/java/com/demo/microservice_2021/elastic/query/service/service/NewsElasticQueryService.java
@@ -3,7 +3,7 @@ package com.demo.microservice_2021.elastic.query.service.service;
 import com.demo.microservice_2021.elastic.model.index.impl.NewsIndexModel;
 import com.demo.microservice_2021.elastic.query.service.api.ElasticDocumentApi;
 import com.demo.microservice_2021.elastic.query.service.model.ElasticQueryServiceResponseModel;
-import com.demo.microservice_2021.elastic.query.service.transformer.ElasticToResponseModelTransformer;
+import com.demo.microservice_2021.elastic.query.service.model.assembler.ElasticQueryServiceResponseModelAssembler;
 import com.demo.microservice_2021.query.client.service.ElasticQueryClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,36 +16,30 @@ public class NewsElasticQueryService implements ElasticQueryService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ElasticDocumentApi.class);
 
-    private final ElasticToResponseModelTransformer elasticToResponseModelTransformer;
     private final ElasticQueryClient<NewsIndexModel> newsIndexModelElasticQueryClient;
+    private final ElasticQueryServiceResponseModelAssembler assembler;
 
-    public NewsElasticQueryService(ElasticToResponseModelTransformer elasticToResponseModelTransformer,
-                                   ElasticQueryClient<NewsIndexModel> newsIndexModelElasticQueryClient) {
-        this.elasticToResponseModelTransformer = elasticToResponseModelTransformer;
+    public NewsElasticQueryService(ElasticQueryClient<NewsIndexModel> newsIndexModelElasticQueryClient,
+                                   ElasticQueryServiceResponseModelAssembler assembler) {
         this.newsIndexModelElasticQueryClient = newsIndexModelElasticQueryClient;
+        this.assembler = assembler;
     }
 
     @Override
     public ElasticQueryServiceResponseModel getDocumentById(Long id) {
         LOG.info("Querying to elasticsearch by id {}", id);
-        return elasticToResponseModelTransformer.getResponseModel(
-                newsIndexModelElasticQueryClient.getIndexModelById(id.toString())
-        );
+        return assembler.toModel(newsIndexModelElasticQueryClient.getIndexModelById(id.toString()));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getDocumentByValue(String value) {
         LOG.info("Querying to elasticsearch by value {}", value);
-        return elasticToResponseModelTransformer.getResponseModels(
-                newsIndexModelElasticQueryClient.getIndexModelByField(value)
-        );
+        return assembler.toModels(newsIndexModelElasticQueryClient.getIndexModelByField(value));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getAllDocument() {
         LOG.info("Querying to elasticsearch for all documents");
-        return elasticToResponseModelTransformer.getResponseModels(
-                newsIndexModelElasticQueryClient.getAllIndexModel()
-        );
+        return assembler.toModels(newsIndexModelElasticQueryClient.getAllIndexModel());
     }
 }
