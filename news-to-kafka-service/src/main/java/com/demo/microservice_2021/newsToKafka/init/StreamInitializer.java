90c7133f3a12163246b1488bb6f18c209398a2cc
mohsen.mahmoudi <mohsen.mahmoudi.91@gmail.com>
2025-03-17 10:25:43 +0330

    refactoring microservices projects and config server port and profile names

--- news-to-kafka-service/src/main/java/com/demo/microservice_2021/newsToKafka/init/StreamInitializer.java
+++
@@ -1,5 +0,0 @@
-package com.demo.microservice_2021.newsToKafka.init;
-
-public interface StreamInitializer {
-    void init();
-}
--- news-to-kafka-service/src/main/java/com/demo/microservice_2021/newsToKafka/service/NewsToKafkaService.java
+++
@@ -1,7 +0,0 @@
-package com.demo.microservice_2021.newsToKafka.service;
-
-import com.demo.microservice_2021.newsToKafka.dto.Article;
-
-public interface NewsToKafkaService {
-    public void streamNewsToKafka(Article article);
-}
